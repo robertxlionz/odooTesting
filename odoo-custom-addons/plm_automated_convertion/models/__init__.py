2998e7587ce3c133b5bccdcbe8fc4fd40524bb75
robertxlionz <robertxlion@gmail.com>
2024-03-18 11:37:37 +0100

    gg

--- odoo-custom-addons/plm_automated_convertion/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import ir_attachment
-from . import plm_convert_servers
-from . import plm_convert_stack
-from . import plm_convert_rule
-from . import product_product
-from . import plm_convert_format
